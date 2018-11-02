920510e7e98ca28ca3b337d7bdd4bf13996113ed
Vladimir Kaltyrin <vkasci@gmail.com>
2018-11-02 16:42:12 +0300

    Removed unused code, disabled trailing_closure rule in swiftlint.yml

--- TodoApplication/TodoApplication/Core/Extensions/UITableViewExtensions.swift
+++ TodoApplication/TodoApplication/Core/Extensions/UITableViewExtensions.swift
@@ -8,14 +8,4 @@ extension UITableView {
             forCellReuseIdentifier: nibName
         )
     }
-
-    func dequeueReusableCell<T: UITableViewCell>(type: T.Type, indexPath: IndexPath) -> T? {
-        let reuseId = String(describing: T.self)
-
-        if let cell = dequeueReusableCell(withIdentifier: reuseId) as? T {
-            return cell
-        }
-
-        return nil
-    }
 }
